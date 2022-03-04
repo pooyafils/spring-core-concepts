30cfd402e376adc3571afa1a7f7a324f7e830959
pooyafils <pooyafils@gmail.com>
2022-03-04 16:29:15 +0330

    inject value from properties file xml

---
+++ src/main/java/injectionofvaluefrompropertiesfile/Coach.java
@@ -0,0 +1,9 @@
+package injectionofvaluefrompropertiesfile;
+
+public interface Coach {
+
+	public String getDailyWorkout();
+	
+	public String getDailyFortune();
+
+}
---
+++ src/main/java/injectionofvaluefrompropertiesfile/FortuneService.java
@@ -0,0 +1,7 @@
+package injectionofvaluefrompropertiesfile;
+
+public interface FortuneService {
+
+	public String getFortune();
+	
+}
